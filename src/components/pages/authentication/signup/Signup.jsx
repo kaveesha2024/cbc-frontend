1691f03f7fabaf50e04939ef88bf0d09adb04892
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-06 20:35:02 +0530

    add Signup route to WebRoutes.

--- src/components/pages/authentication/signup/Signup.jsx
+++ src/components/pages/authentication/signup/Signup.jsx
@@ -1,7 +1,7 @@
 const Signup = () => {
     return(
         <div>
-            test
+            
         </div>
     );
 };
--- src/components/utility/routes/WebRoutes.jsx
+++ src/components/utility/routes/WebRoutes.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router"
 import App from "../../../App.jsx";
+import Signup from "../../pages/authentication/signup/Signup.jsx";
 
 const WebRoutes = () => {
     return ( 
         <Routes>
             <Route path="/" element={<App />} />
+            <Route path="/authentication/signup" element={<Signup />} />
         </Routes>
      );
 }
