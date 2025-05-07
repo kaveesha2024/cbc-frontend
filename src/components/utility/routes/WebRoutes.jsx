10ff12d12e251a1cc01e78b0176707802214c0c7
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-07 11:22:45 +0530

    customize sign in page.

--- src/components/utility/routes/WebRoutes.jsx
+++ src/components/utility/routes/WebRoutes.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router"
 import App from "../../../App.jsx";
-import Signup from "../../pages/authentication/signup/Signup.jsx";
+import SignIn from "../../pages/authentication/signin/SignIn.js";
 
 const WebRoutes = () => {
     return ( 
         <Routes>
             <Route path="/" element={<App />} />
-            <Route path="/authentication/signup" element={<Signup />} />
+            <Route path="/authentication/signup" element={<SignIn />} />
         </Routes>
      );
 }
