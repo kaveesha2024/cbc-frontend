f2ac130e8aca0e8ab2314585519833dc4d798fc1
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-18 21:47:36 +0530

    Add wishlist functionality to HomeProductContainer

--- src/components/homeProductContainer/HomeProductContainer.jsx
+++ src/components/homeProductContainer/HomeProductContainer.jsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router";
+import { FaHeart } from "react-icons/fa";
+import toggleWishlist, { isInWishlist } from "../wishList/HandleWishList";
 const HomeProductContainer = ({ products }) => {
     const navigate = useNavigate();
     return (
         <div className="w-full my-20 flex flex-wrap justify-center gap-10 items-center">
             {
                 products.length > 0 && products.map((product, index) => (
-                    <div key={index} onClick={() => { navigate(`view/product?id=${product.productId}`) }} className="w-[320px] h-[550px] shadow hover:shadow-2xl  transition duration-200 cursor-pointer p-2 rounded-lg">
+                    <div key={index} className="w-[320px] h-[550px] shadow hover:shadow-2xl  transition duration-200 p-2 rounded-lg">
                         <div className="rounded-lg w-full mb-5 h-[50%]">
-                            <img className="max-h-full min-h-full max-w-full min-w-full" src={product.images[0]} alt="image" />
+                            <img onClick={() => { navigate(`view/product?id=${product.productId}`) }} className="max-h-full min-h-full cursor-pointer max-w-full min-w-full" src={product.images[0]} alt="image" />
                         </div>
                         <div className="flex flex-col gap-3">
                             <p className="font-bold text-2xl">{product.name}</p>
@@ -16,7 +18,14 @@ const HomeProductContainer = ({ products }) => {
                                 <span className="text-accent text-xl font-bold">LKR {product.price}.00</span>
                                 <span className="line-through font-thin">LKR {product.labelledPrice}.00</span>
                             </div>
-                            <div className="flex justify-end ">
+                            <div className="flex justify-between items-center">
+                                {
+                                    isInWishlist(product.productId) ? (
+                                        <button onClick={() => {toggleWishlist(product.productId)}} className="cursor-pointer text-2xl text-accent "><FaHeart /></button>
+                                    ) : (
+                                        <button onClick={() => {toggleWishlist(product.productId)}} className="cursor-pointer text-secondary text-2xl hover:text-accent "><FaHeart /></button>
+                                    )
+                                }
                                 <button className="bg-accent p-2 text-primary rounded-sm cursor-pointer">Add to cart</button>
                             </div>
                         </div>
